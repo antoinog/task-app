861471e7d622c834e76aaa119884e3f170ee3c91
Anthony Cunningham <anthoncu@ie.ibm.com>
2023-03-02 16:05:57 +0000

    add Column component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import initialData from "./initial-data";
+import Column from './column'
+class App extends React.Component {
+    state = initialData;
 
-const App =()=>"Hello world"
+    render() {
+        return this.state.columnOrder.map(
+            columnId => {
+                const column = this.state.columns[columnId];
+                const tasks = column.taskIds.map(
+                    taskId => this.state.tasks[taskId]
+                );
+                return <Column key={column.id} column={column} tasks={tasks}/>
+            }
+        )
+    }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
