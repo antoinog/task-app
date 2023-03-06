bc5306d715041cd9a4c19b2be18eab4d8cc622ac
Anthony Cunningham <anthoncu@ie.ibm.com>
2023-03-06 10:00:32 +0000

    rename

--- src/column.jsx
+++ src/column.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import styled from "styled-components";
 import Task from "./task";
-const Container=styled.div`
-border-style: ridge;
+import {Droppable} from "react-beautiful-dnd";
+
+const Container = styled.div`
+  border-style: ridge;
   border: brown;
   border-width: thick;
   margin: 8px;
 `;
-const Title=styled.h3``;
-const TaskList=styled.div``;
-export default class Column extends  React.Component{
+const Title = styled.h3``;
+const TaskList = styled.div``;
+export default class Column extends React.Component {
     render() {
         return (
             <Container>
                 <Title>{this.props.column.title}</Title>
-                <TaskList>
-                    {this.props.tasks.map(
-                        task=><Task key={task.id} task={task} />
+                <Droppable droppableId={this.props.column.id}>
+                    {(provided) => (
+                        <TaskList
+                            innerRef={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            {this.props.tasks.map(
+                                (task, index) =>
+                                    <Task key={task.id} task={task} index={index}/>
+                            )}
+                            {provided.placeholder}
+                        </TaskList>
                     )}
-                </TaskList>
+                </Droppable>
             </Container>
         );
     }
--- src/task.jsx
+++ src/task.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import styled from "styled-components";
-const TaskBox=styled.div`
-    border: aqua;
+import {Draggable} from "react-beautiful-dnd";
+
+const TaskBox = styled.div`
+  border: aqua;
   border-style: dashed;
 `;
-export default class Task extends React.Component{
-    render(){
-        return <TaskBox>{this.props.task.content}</TaskBox> ;
+export default class Task extends React.Component {
+    render() {
+        return (
+            <Draggable
+                draggableId={this.props.task.id}
+                index={this.props.index}
+            >
+                {(provided) => (
+                    <TaskBox
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        innerRef={provided.innerRef}
+                    >
+                        {this.props.task.content}</TaskBox>
+                )
+                }
+
+            </Draggable>
+        );
     }
 }
